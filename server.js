547efb644490e0d8ae1f15717d9c38338107ddc1
maxajwang-ke <maxajwang@gmail.com>
2023-02-27 17:34:59 +0300

    project folder/file structure

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(initial_path, "home.html"));
 })
 
+app.get('/editor', (req, res) => {
+    res.sendFile(path.join(initial_path, "editor.html"));
+})
+
 app.listen("3000", () => {
     console.log('listening......');
 })
