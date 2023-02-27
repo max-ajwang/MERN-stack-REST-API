0e4e87031deb00e8cc034ba229e418dd1751e215
maxajwang-ke <maxajwang@gmail.com>
2023-02-27 18:56:36 +0300

    image upload css and js

--- server.js
+++ server.js
@@ -16,6 +16,25 @@ app.get('/editor', (req, res) => {
     res.sendFile(path.join(initial_path, "editor.html"));
 })
 
+app.post('/upload', (req, res) => {
+    let file = req.files.image;
+    let date = new Date();
+    // image name
+    let imagename = date.getDate() + date.getTime() + file.name;
+    // image upload path
+    let path = 'public/uploads/' + imagename;
+
+    // create upload
+    file.mv(path, (err, result) => {
+        if(err){
+            throw err;
+        } else{
+            // our image upload path
+            res.json(`uploads/${imagename}`)
+        }
+    })
+})
+
 app.listen("3000", () => {
     console.log('listening......');
 })
