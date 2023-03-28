e76145b778fe92cae0e8fd213cb56fa5a9d70479
max-ajwang <maxajwang@gmail.com>
2023-03-28 23:46:11 +0300

    removed all boilerplate code and created first react component

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//import ES6 modules
+import React from "react"; 
+import ReactDom from "react-dom";
+//create function based component and capitalize the first letter of the function name. 
+//return JSX (html to be displayed in index.html)
+function Greeting(){
+  return <h4>Max, first component</h4>; 
+}
+//Render the component to 'root' in index.html by passing two arguments to the render property i.e 
+//component to be rendered and where it will be rendered
+ReactDom.render(<Greeting/>, document.getElementById('root')); 
