20ee40d09cf2776dabec1d52e04af6261c156813
maxajwang-ke <maxajwang@gmail.com>
2023-04-01 05:34:01 +0300

    react frontend update 1

--- client/src/index.js
+++ client/src/index.js
@@ -3,13 +3,21 @@ import React from "react";
 import ReactDom from "react-dom";
 //create function based component and capitalize the first letter of the function name. 
 //return JSX (html to be displayed in index.html)
-function Greeting(){
+function App () {
   return (
     <div>
-      <h4>Max, first component</h4>
+      <Person />
+      <Message />
     </div>
   );
 }
+
+const Person = () => <h2>Max Ajwang'</h2>;
+
+const Message = () => {
+  return <p>Stables - Premiering Soon!! August 2023</p>
+};
+
 //Render the component to 'root' in index.html by passing two arguments to the render property i.e 
 //component to be rendered and where it will be rendered
-ReactDom.render(<Greeting/>, document.getElementById('root')); 
+ReactDom.render(<App/>, document.getElementById('root')); 
