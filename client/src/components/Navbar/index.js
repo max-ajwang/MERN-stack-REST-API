402141e53dbf99cce29a9c4c56da30f9c4da5b7d
max-ajwang <maxajwang@gmail.com>
2023-04-06 03:08:36 +0300

    Nav bar component

---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+  Nav,
+  NavLogo,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
+} from './NavbarElements';
+  
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavLogo to='/'>
+          Max Ajwang'
+        </NavLogo>
+
+        <Bars />
+        
+        <NavMenu>
+          <NavLink to='/' activeStyle>
+            Home
+          </NavLink> 
+          <NavLink to='/about' activeStyle>
+            About
+          </NavLink>          
+          <NavLink to='/blogs' activeStyle>
+            Blogs
+          </NavLink>
+          <NavLink to='/sign-up' activeStyle>
+            Sign Up
+          </NavLink>
+          {/* Second Nav */}
+          {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+        </NavBtn>
+      </Nav>
+    </>
+  );
+};
+  
+export default Navbar;
--- client/src/index.js
+++ client/src/index.js
@@ -4,26 +4,45 @@ import ReactDom from "react-dom";
 
 //CSS
 import './index.css'
-import {books} from './books'
-import Book from './Book'
+import {books} from './components/Book/books'
+import Book from './components/Book/Book'
+
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Blogs from './pages/blogs';
+import SignUp from './pages/signup';
 
 //create function based component and capitalize the first letter of the function name. 
 //return JSX (html to be displayed in index.html)
 function App () {
   return (
-    <section className="booklist">
-      {books.map((book) => {
-        return (
-          <Book key={book.id} book={book}>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/blogs' component={Blogs} />
+          <Route path='/sign-up' component={SignUp} />
+        </Routes>
+      </Router>
 
-          </Book>
-        );
-      })}
-    </section>
+      <section className="booklist">
+        {books.map((book) => {
+          return (
+            <Book key={book.id} book={book}>
+
+            </Book>
+          );
+        })}
+      </section>
+    </>
   );
 }
 
-/*const Person = () => <h2>Max Ajwang'</h2>;
+/*const Person = () => ;
 
 const Message = () => {
   return <p>Stables - Premiering Soon!! August 2023</p>
