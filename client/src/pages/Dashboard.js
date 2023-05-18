ed2a09f7453f08b1f6fdc03c170b805e146f8064
max-ajwang <maxajwang@gmail.com>
2023-05-18 06:35:32 +0300

    Error Page - Pending CSS/styled component

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Navbar, Footer } from '../components'
 
 const Dashboard = () => {
   return (
-    <h1>Dashboard</h1>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+      </div>
+      <Footer />
+    </div>
   )
 }
 
