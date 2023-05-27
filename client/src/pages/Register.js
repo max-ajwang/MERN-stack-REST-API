29c94dadd8fdb1ba25d4780260bac5ff1cb7cae4
max-ajwang <maxajwang@gmail.com>
2023-05-27 22:59:17 +0300

    Added Register page and RegisterPage Wrappper, return statement for Register page is incomplete

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import {Logo} from '../components'
+import Wrapper from '../assets/wrappers/RegisterPage'
+
+const initialState = {
+  name:'',
+  email:'',
+  password:'',
+  isMember:true,
+}
 
 const Register = () => {
+  const [values, setValues] = useState(initialState)
+  //global state and useNavigate
+
+  const handleChange = (e) => {
+    console.log(e.target)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log(e.target)
+  }
+  
   return (
-    <h1>Register</h1>
+    <Wrapper>
+      <form className='form' onSubmit={onSubmit}>
+        <Logo />
+      </form>
+    </Wrapper>
   )
 }
 
