0db42598fc5e8c2850ce8dbeeae2b9187d449272
max-ajwang <maxajwang@gmail.com>
2023-06-15 01:31:45 +0300

    DONE: Media queries for Header component wrapper

--- client/src/assets/wrappers/Header.js
+++ client/src/assets/wrappers/Header.js
@@ -1,61 +1,49 @@
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
-  @media screen and (max-width: 480px) {
-    .page {
-      min-height: cals(100vh - var(--nav-height));
-      display: block;
-      grid-template-columns: 1fr;
-      align-items: center;
+  h1 {
+    font-weight: 700;
+    font-family: var(--bodyFont);
+    span {
+      color: var(--primary-500);
     }
-      
-    .main-img {
-      display: none;
-    }
+  }
+
+  .page {
+    min-height: cals(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    grid-template-columns: 1fr 1fr;
+    column-gap: 3rem;
+  }
+
+  @media screen and (max-width: 1280px) {
+  }
+
+  @media screen and (max-width: 1024px) {
   }
 
   @media screen and (max-width: 768px) {
     .page {
       min-height: cals(100vh - var(--nav-height));
-      display: grid;
+      display: block;
       align-items: center;
     }
-  
+
     .main-img {
       display: none;
     }
   }
 
-  @media screen and (max-width: 1024px) {
-    .page {
-      grid-template-columns: 1fr 1fr;
-      column-gap: 3rem;
-    }
-
-    .main-img {
-      display: block;
-    }
-  }
-
-  @media screen and (max-width: 1280px) {
+  @media screen and (max-width: 480px) {
     .page {
       min-height: cals(100vh - var(--nav-height));
-      display: grid;
-      align-items: center;
-      grid-template-columns: 1fr 1fr;
-      column-gap: 3rem;
-    }
-
-    h1 {
-      font-weight: 700;
-      font-family: var(--bodyFont);
-      span {
-        color: var(--primary-500);
-      }
-    }
-
-    .main-img {
       display: block;
+      align-items: center;
+    }
+      
+    .main-img {
+      display: none;
     }
   }
 `
