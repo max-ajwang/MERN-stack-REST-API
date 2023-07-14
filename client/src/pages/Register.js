9e29eea3b81772aeabc095420d2a8d975169a352
max-ajwang <maxajwang@gmail.com>
2023-07-14 20:17:26 +0300

    Display alert - compete

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -13,7 +13,7 @@ const initialState = {
 const Register = () => {
   const [values, setValues] = useState(initialState)
   //global state and useNavigate
-  const {isLoading, showAlert} = useAppContext()
+  const {isLoading, showAlert, displayAlert} = useAppContext()
 
 
   const toggleMember = () => {
@@ -21,12 +21,17 @@ const Register = () => {
   }
 
   const handleChange = (e) => {
-    console.log(e.target)
+    setValues({...values, [e.target.name]: e.target.value})
   }
 
   const onSubmit = (e) => {
     e.preventDefault()
-    console.log(e.target)
+    const { name, email, password, isMember } = values
+    if (!email || !password || (!isMember && !name)) {
+      displayAlert()
+      return
+    }
+    console.log(values)
   }
   
   return (
