1f8882eae5aca4aef2a1602251ad834426d7679d
max-ajwang <maxajwang@gmail.com>
2023-07-14 19:39:29 +0300

    Display alert setup

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -2,6 +2,8 @@ import React, {useState, useReducer, useContext} from 'react'
 
 import reducer from './reducer'
 
+import { DISPLAY_ALERT } from "./actions"
+
 const initialState = {
     isLoading: false,
     showAlert: false,
@@ -14,8 +16,12 @@ const AppContext = React.createContext()
 const AppProvider = ({children}) => {
     const [state, dispatch] = useReducer(reducer, initialState)
 
+    const displayAlert = () => {
+        dispatch({type: DISPLAY_ALERT})
+    }
+
     return (
-        <AppContext.Provider value={{...state}}>
+        <AppContext.Provider value={{...state, displayAlert}}>
             {children}
         </AppContext.Provider>
     )
