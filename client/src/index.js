7744ab7c85dd31df142f48ef809474215849f16a
max-ajwang <maxajwang@gmail.com>
2023-07-14 03:48:21 +0300

    Global context

--- client/src/index.js
+++ client/src/index.js
@@ -5,8 +5,16 @@ import 'normalize.css'
 //CSS
 import './index.css';
 import App from './App';
+import { AppProvider } from "./context/appContext";
 
 
 //Render the component to 'root' in index.html by passing two arguments to the render property i.e 
 //component to be rendered and where it will be rendered
-ReactDom.render(<App/>, document.getElementById('root')); 
+ReactDom.render(
+    <React.StrictMode>
+        <AppProvider>
+            <App />
+        </AppProvider>
+    </React.StrictMode>, 
+    document.getElementById('root')
+); 
