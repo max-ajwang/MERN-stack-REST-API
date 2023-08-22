79972e47a4e62a0f8eed38bc4f1edfba7e7008d5
max-ajwang <maxajwang@gmail.com>
2023-08-22 19:42:58 +0300

    Added wrappers and completed demo Landing Page

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,11 +1,1 @@
-import Alert from './Alert'
-import Article from './Article'
-import Blog from './Blog'
-import Footer from './Footer'
-import FormRow from './FormRow'
-import Header from './Header'
-import Logo from './Logo'
-import Navbar from './Navbar'
-
-export {Navbar, Logo, Article, Blog, Footer, Header, FormRow, Alert}
-
+export { default as Logo } from "./Logo";
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,14 +1,34 @@
-import { Navbar, Header, Blog, Footer } from '../components'
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Link } from "react-router-dom";
+import { Logo } from "../components";
 
 const Landing = () => {
   return (
-    <main>
-      <Navbar />
-      <Header />
-      <Blog />
-      <Footer />
-    </main>
-  )
-}
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            Road to <span>$1K in MRR: </span> the App
+          </h1>
+          <p>
+            Hey there, I'm Max. I'm a cyber security analyst and professional
+            web developer based out of Nairobi, Kenya. I'm building a portfolio
+            of internet businesses here and sharing my journey to $1K in monthly
+            recurrent revenue. Follow my journey here...
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn login-link">
+            Login / Demo User
+          </Link>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 
-export default Landing
+export default Landing;
